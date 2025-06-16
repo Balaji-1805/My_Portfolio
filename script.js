6782a9d8fb5a6d7377258602291c86a0253aadaf
Balaji-1805 <ugginabalaji2003@gmail.com>
2025-06-16 23:14:25 +0530

    Removing fetch from js

--- script.js
+++ script.js
@@ -2,18 +2,10 @@ const form=document.getElementById("contactForm");
 
 form.addEventListener('submit',function(e)
 {
-   e.preventDefault();
-   const formdata=new FormData(this);
-   fetch("https://formsubmit.co/ugginabalaji2003@gmail.com",{
-    method:"POST",
-    body:formdata,
-    headers:{Accept:"application/json"},
-   }).then((res)=>{
+  
         const toast=document.getElementById('toast');
         toast.classList.add("show");
         setTimeout(()=>{
             toast.classList.remove("show");
         },3000);
-        form.reset();
-   });
 });
