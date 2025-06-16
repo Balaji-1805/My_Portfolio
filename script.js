e254730e865ee82ad12898aef00e2bddd33c262a
Balaji-1805 <ugginabalaji2003@gmail.com>
2025-06-16 23:36:09 +0530

    Final Script commit

--- script.js
+++ script.js
@@ -1,13 +1,5 @@
 const form=document.getElementById("contactForm");
 form.addEventListener('submit',function(e)
 {
-  
-     setTimeout(() => {
-    const toast = document.getElementById("toast");
-    toast.classList.add("show");
-
-    setTimeout(() => {
-      toast.classList.remove("show");
-    }, 3000);
-  }, 500);
+    alert("Message Sent successfully!");
 });
