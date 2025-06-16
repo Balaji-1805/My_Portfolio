475978038962287045d55625ba18dbf8f40d1951
Balaji-1805 <ugginabalaji2003@gmail.com>
2025-06-16 22:59:08 +0530

    Added toast animation

--- script.js
+++ script.js
@@ -1,10 +1,19 @@
-const toastMessage=document.getElementById('toast');
 const form=document.getElementById("contactForm");
 
 form.addEventListener('submit',function(e)
 {
-    toastMessage.className="show";
-    setTimeout(()=>{
-        toastMessage.className=toastMessage.className.replace("show","");
-    },3000);
-})
+   e.preventDefault();
+   const formdata=new FormData(this);
+   fetch("https:formsubmit.co/ugginabalaji2003@gmail.com",{
+    method:"POST",
+    body:formdata,
+    headers:{Accept:"application/json"},
+   }).then((res)=>{
+        const toast=document.getElementById('toast');
+        toast.classList.add("show");
+        setTimeout(()=>{
+            toast.classList.remove("show");
+        },3000);
+        this.reset();
+   });
+});
